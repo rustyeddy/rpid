f7ee834b0eb3a7202c5c23ee01a4950de80bb267
Rusty Eddy <rusty@rustyeddy.com>
2019-04-05 13:22:40 -0700

    added a home page

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html', title="Mobile Robot I/O")
+
+@app.route('/hello')
 def hello_world():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/routes')
+def display_routes():
+    print(url_for('index'))
+    print(url_for('login'))
