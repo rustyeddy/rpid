6398d794b60d2a4b5d9cd6c5d2003428ff0579ba
Rusty Eddy <re@mobilerobot.io>
2019-04-07 15:54:17 -0700

    We are now streaming video

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask import render_template
+from flask import Response
+import camera
+import time
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,3 +18,14 @@ def hello_world():
 def display_routes():
     print(url_for('index'))
     print(url_for('login'))
+
+@app.route("/image")
+def image_server():
+    return render_template('image_server.html')
+    
+@app.route("/display")
+def display():
+    return Response(camera.frame_generator(),
+            mimetype='multipart/x-mixed-replace; boundary=frame')
+
+app.run(host="0.0.0.0", debug=True, port=5001)
