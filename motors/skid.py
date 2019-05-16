62c28a89c2f5b7ca1c5423ed9c216458c883c65b
Rusty Eddy <re@mobilerobot.io>
2019-05-15 18:52:28 -0700

    motor is controlled via r/l string

--- motors/skid.py
+++ motors/skid.py
@@ -1,28 +1,38 @@
 from adafruit_motorkit import MotorKit
 import time
 
-skid = MotorKit()
-
 def skid_set_throttle(vehicle, left, right):
     assert -1.0 <= float(left) <= 1.0
     assert -1.0 <= float(right) <= 1.0
     vehicle.motor1.throttle = left
     vehicle.motor2.throttle = right
 
-while True:
-    instr = input("devctl~> ")
-    if instr == "":
-        continue
+if __name__ == "__main__":
 
-    vals = instr.split("/")
-    if len(vals) != 2:
-        print("ERROR expected two values")
-        continue
+    skid = MotorKit()
+    while True:
 
-    left = float(vals[0])
-    right = float(vals[1])
-    assert -1.0 <= right <= 1.0
-    assert -1.0 <= left <= 1.0
-    print("Fixin to set throttle " + str(left) + " " + str(right))
+        # wait for stdin to tell us what to do
+        instr = input("devctl~> ")
+        if instr == "":
+            continue
 
-    skid_set_throttle(skid, left, right)
+        # the command looks like the '<r>/<l>' where <r> and <l> are floating
+        # point numbers between -1.0 and 1.0 inclusive. r stands for right, 
+        # l stands for left
+        vals = instr.split("/")
+        if len(vals) != 2:
+            print("ERROR expected two values")
+            continue
+
+        # convert the parameter strings to floats
+        left = float(vals[0])
+        right = float(vals[1])
+
+        # make sure we don't dive our motors something stupid
+        assert -1.0 <= right <= 1.0
+        assert -1.0 <= left <= 1.0
+
+        print("Fixin to set throttle " + str(left) + " " + str(right))
+
+        skid_set_throttle(skid, left, right)
