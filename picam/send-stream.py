dc52530f81386d2ff8c5df4b0cb3e507446754a9
Rusty Eddy <re@mobilerobot.io>
2019-05-21 23:15:57 -0700

    committed frame 232

--- picam/send-stream.py
+++ picam/send-stream.py
@@ -6,8 +6,8 @@ import picamera
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 client_socket = socket.socket()
-# client_socket.connect(('10.24.0.11', 8000))
-client_socket.connect(('10.24.2.19', 8000))
+client_socket.connect(('10.24.0.11', 8000))
+# client_socket.connect(('10.24.2.19', 8000))
 
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
